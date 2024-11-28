727bb6874474682716432c8c18ea843ac8712202
zhangyu <zhangyud@uniontech.com>
2024-11-27 22:37:42 -0700

    feat: update kf6-milou to 6.2.1-1deepin1

---
+++ lib/mousehelper.cpp
@@ -0,0 +1,66 @@
+/*
+ * SPDX-FileCopyrightText: 2017 Peifeng Yu <aetf@unlimitedcodeworks.xyz>
+ *
+ * SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+ *
+ */
+
+#include "mousehelper.h"
+
+#include <QCursor>
+#include <QMimeData>
+#include <QUrl>
+
+using namespace Milou;
+
+MouseHelper::MouseHelper(QObject *parent)
+    : QObject(parent)
+{
+}
+
+MouseHelper::~MouseHelper()
+{
+}
+
+QPointF MouseHelper::globalMousePosition() const
+{
+    return QCursor::pos();
+}
+
+QVariantMap MouseHelper::generateMimeDataMap(QMimeData *data) const
+{
+    QVariantMap dataMap;
+
+    QStringList formats = data->formats();
+    auto it = std::find(formats.cbegin(), formats.cend(), QLatin1String("text/plain"));
+    if (it != formats.cend()) {
+        dataMap.insert(*it, data->text());
+        formats.erase(it);
+    }
+
+    it = std::find(formats.cbegin(), formats.cend(), QLatin1String("text/html"));
+    if (it != formats.cend()) {
+        dataMap.insert(*it, data->html());
+        formats.erase(it);
+    }
+
+    it = std::find(formats.cbegin(), formats.cend(), QLatin1String("text/uri-list"));
+    if (it != formats.cend()) {
+        const QList<QUrl> urls = data->urls();
+        dataMap.insert(*it, QVariantList(urls.cbegin(), urls.cend()));
+    }
+
+    it = std::find_if(formats.cbegin(), formats.cend(), [](const QString &format) {
+        return format.startsWith(QLatin1String("image/"));
+    });
+    if (it != formats.cend()) {
+        dataMap.insert(*it, data->imageData());
+        formats.erase(it);
+    }
+
+    for (const QString &format : std::as_const(formats)) {
+        dataMap.insert(format, data->data(format));
+    }
+
+    return dataMap;
+}
---
+++ lib/resultsmodel.cpp
@@ -0,0 +1,34 @@
+/*
+ * SPDX-FileCopyrightText: 2019 Kai Uwe Broulik <kde@broulik.de>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+ */
+
+#include "resultsmodel.h"
+
+using namespace Milou;
+
+using namespace Qt::StringLiterals;
+
+ResultsModel::ResultsModel(QObject *parent)
+    : KRunner::ResultsModel(parent)
+{
+    const auto config = KSharedConfig::openConfig(u"krunnerrc"_s);
+    m_configWatcher = KConfigWatcher::create(config);
+    const auto assignFavoriteIds = [this, config]() {
+        const KConfigGroup grp = config->group(u"General"_s).parent().group(u"Plugins"_s).group(u"Favorites"_s);
+        m_configFavoriteIds = grp.readEntry("plugins", QStringList{u"krunner_services"_s, u"krunner_systemsettings"_s});
+        if (!m_favoritesExplicitlySet) {
+            KRunner::ResultsModel::setFavoriteIds(m_configFavoriteIds);
+        }
+    };
+    connect(m_configWatcher.data(), &KConfigWatcher::configChanged, this, [assignFavoriteIds](const KConfigGroup &group) {
+        const QLatin1String pluginsGrp("Plugins");
+        if (group.name() == QLatin1String("Favorites") && group.parent().name() == pluginsGrp) {
+            assignFavoriteIds();
+        }
+    });
+    assignFavoriteIds();
+}
+
+#include "moc_resultsmodel.cpp"
